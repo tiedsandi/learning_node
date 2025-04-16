b868261eac8138db65a58f504fc4bbb6ef914eee
tiedsandi <fachransandi@gmail.com>
2025-04-16 19:24:35 +0700

    02- error delete todolist

--- 02-Todolist_RESTful_API/todolist-service.mjs
+++ 02-Todolist_RESTful_API/todolist-service.mjs
@@ -42,6 +42,19 @@ export class TodolistService {
       response.end();
     });
   }
+
+  deleteTodo(request, response) {
+    request.addListener("data", (data) => {
+      const body = JSON.parse(data.toString());
+
+      if (this.todolist[body.id]) {
+        this.todolist.splice(body.id, 1);
+      }
+
+      response.write(this.getJsonTodoList());
+      response.end();
+    });
+  }
 }
 
 // export class TodolistService {
